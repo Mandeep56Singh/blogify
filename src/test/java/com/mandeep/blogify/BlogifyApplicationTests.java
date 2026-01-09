fb6d688914b0bad689fcb16f25ec713523e60707
mandeep <mandeepraj2312@gmail.com>
2026-01-09 20:21:30 +0530

    chore: initial project setup

---
+++ src/test/java/com/mandeep/blogify/BlogifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandeep.blogify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
